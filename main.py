9483b3cca20061dfe0c83822a791aeb3a67e2cc7
harpreet kaur <126206360+simmi123455@users.noreply.github.com>
2023-02-26 01:04:54 +0000

    added HTML and CSS

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 #importing functionality from flask and putting it into variable app.
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return f"hello_world"
+  return render_template('home.html')
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
